d92b82772e9b96296c3f156ef0ef9c171123edd5
AZhDevelop <azh.develop@mail.ru>
2024-06-03 14:49:03 +0300

    Second homework commit

--- app/src/main/java/com/example/sprinteighthomeworkone/MainActivity.kt
+++ app/src/main/java/com/example/sprinteighthomeworkone/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.example.sprinteighthomeworkone
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        /** Реализация нажатия на кнопку "Поиск" с помощью анонимного класса */
+
+        val searchButton = findViewById<Button>(R.id.search)
+
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Нажали на кнопку \"Поиск\"",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        /** Реализация нажатия на кнопку "Медиатека" с помощью лямбда-выражения */
+
+        val mediaButton = findViewById<Button>(R.id.media)
+        mediaButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Нажали на кнопку \"Медиатека\"",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        /** Реализация нажатия на кнопку "Настройки" с помощью MainActivity */
+
+        val settingsButton = findViewById<Button>(R.id.settings)
+        settingsButton.setOnClickListener(this@MainActivity)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.settings -> {
+                Toast.makeText(this@MainActivity,
+                    "Нажали на кнопку \"Настройки\"",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
